7dbcc4c296a3a1e68d2e4c892b55484b6f644337
MastaMoose <mustafa.sheikhj@gmail.com>
2018-12-22 13:01:38 -0500

    Added some trial scripts for checking out Matplotlib_graphing.py

--- Matplotlib_graphing.py
+++ Matplotlib_graphing.py
@@ -4,14 +4,26 @@ Created on Wed Dec 12 21:25:55 2018
 Purpose: Just the pyplot/matplotlib turotial
 @author: Mustafa
 """
-
 import numpy as np
 import matplotlib.pyplot as plt
 
-X = np.linspace(-np.pi, np.pi, 256, endpoint=True)
-C, S = np.cos(X), np.sin(X)
+plt.figure(figsize=(8,6), dpi=80)
 
-plt.plot(X,C)
-plt.plot(X,S)
+plt.subplot(111)
+
+X = np.linspace(-np.pi, np.pi, 256, endpoint = True)
+C,S = np.cos(X), np.sin(X)
+
+plt.plot(X, C, color = 'blue', linewidth=1.0, linestyle="-")
+
+plt.plot(X, S, color="green", linewidth=1.0, linestyle="-")
+
+plt.xlim(-4.0,4.0)
+
+plt.xticks(np.linspace(-4,4,9,endpoint=True))
+
+plt.ylim(-1.0,1.0)
+
+plt.yticks(np.linspace(-1,1,5,endpoint=True))
 
 plt.show()
